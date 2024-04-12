706de3c2b5eae8d85ec99ffc272b0b74aca0b5e7
sabrinaallgalvao <sabrinaallgalvao@gmail.com>
2024-04-12 02:23:21 -0300

    Estrutura da pag inicial

---
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult GrupoProduto()
+        {
+            return View();
+        }
+
+        public IActionResult MarcaProduto()
+        {
+            return View();
+        }
+
+        public IActionResult LocalProduto()
+        {
+            return View();
+        }
+
+        public IActionResult Produto()
+        {
+            return View();
+        }
+
+        public IActionResult Pais()
+        {
+            return View();
+        }
+
+        public IActionResult Estado()
+        {
+            return View();
+        }
+
+        public IActionResult Cidade()
+        {
+            return View();
+        }
+
+        public IActionResult Fornecedor()
+        {
+            return View();
+        }
+
+        public IActionResult PerfilUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult Usuario()
+        {
+            return View();
+        }
+
+    }
+}
